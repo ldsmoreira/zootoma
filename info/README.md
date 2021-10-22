af20f60c74dac08f2adb3434e739f3949c36a5b3
moreira0102 <lucasmoreira@poli.ufrj.br>
2021-10-22 13:58:35 -0300

    main

---
+++ info/README.md
@@ -0,0 +1,5 @@
+# Repositories
+
+### [go-basics] (https://github.com/gophertuts/go-basics)
+
+On that repository we can get golang basic information and code snippets to start design and developing toma application with the golang programming language
---
+++ test/experimentation/server/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	connHost = "localhost"
+	connPort = "9000"
+	connType = "tcp"
+)
+
+func main() {
+	fmt.Println("Starting " + connType + " server on " + connHost + ":" + connPort)
+	l, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error connecting:", err.Error())
+			return
+		}
+		fmt.Println("Client connected.")
+
+		fmt.Println("Client " + c.RemoteAddr().String() + " connected.")
+
+		go handleConnection(c)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	buffer, err := bufio.NewReader(conn).ReadBytes('\n')
+
+	if err != nil {
+		fmt.Println("Client left.")
+		conn.Close()
+		return
+	}
+
+	log.Println("Client message:", string(buffer[:len(buffer)-1]))
+
+	conn.Write(buffer)
+
+	handleConnection(conn)
+}
