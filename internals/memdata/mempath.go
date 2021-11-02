9ab3510467a86993088b795a9b5e047758216e5e
moreira0102 <lucasmoreira@poli.ufrj.br>
2021-11-02 20:58:42 -0300

    Documentation added in mempath.go

--- internals/memdata/mempath.go
+++ internals/memdata/mempath.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+//check funtion checks if there are errors
 func check(e error) {
 
 	if e != nil {
@@ -13,15 +14,18 @@ func check(e error) {
 
 }
 
+//MemPath is the basic data element of tomazoo
 type MemPath struct {
-	Data []byte
-	Path string
+	Data []byte //Data persisted in memory
+	Path string //Path string with the path to the persisted data
 }
 
+//PathMap is the basic data structure to manage MemPath objects
 type PathMap struct {
-	Pathmap map[string]*MemPath
+	Pathmap map[string]*MemPath //MemPath is the Hash Map used to find data in O(1)
 }
 
+//NewPathMap is the constructor of the PathMap objects
 func NewPathMap() *PathMap {
 
 	pathmap := new(PathMap)
@@ -30,10 +34,13 @@ func NewPathMap() *PathMap {
 
 }
 
+//GetMemPath is a method of PathMap that return the MemPath object
+//associated with the path string
 func (pathmap PathMap) GetMemPath(path string) *MemPath {
 	return pathmap.Pathmap[path]
 }
 
+//SetPathMap is a method that set a key of path inside PathMap
 func (pathmap PathMap) SetMemPath(path string) *MemPath {
 	p := new(MemPath)
 	p.Path = path
@@ -41,6 +48,7 @@ func (pathmap PathMap) SetMemPath(path string) *MemPath {
 	return p
 }
 
+//NewPath is the constructor of MemPath
 func NewPath(path string, mapper *PathMap) *MemPath {
 
 	newpath := mapper.SetMemPath(path)
@@ -48,6 +56,7 @@ func NewPath(path string, mapper *PathMap) *MemPath {
 
 }
 
+//ShowPath shows the path associated with MemPath object
 func (path MemPath) ShowPath() {
 
 	fmt.Println(path.Path)
