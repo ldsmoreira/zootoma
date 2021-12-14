5460234a2df8d99439d2bdfcefa647cc00c54970
moreira0102 <lucasmoreira@poli.ufrj.br>
2021-12-14 06:22:49 -0300

    command line flags

--- cmd/zootoma/main.go
+++ cmd/zootoma/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/moreira0102/zootoma/internal/server/server"
 )
 
 const (
-	HOST      = "localhost"
-	PORT      = "9000"
-	CONN_TYPE = "tcp"
+	HOST = "localhost"
 )
 
 func main() {
-	server.StartListen(HOST, PORT, CONN_TYPE)
+
+	PORT := flag.String("port", "9009", "The port to bind zootoma")
+	CONN_TYPE := flag.String("conntype", "tcp", "The transport layer protocol used by zootoma")
+
+	flag.Parse()
+
+	server.StartListen(HOST, *PORT, *CONN_TYPE)
 }
